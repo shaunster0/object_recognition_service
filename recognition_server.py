3ae1ba1b7557cf17c6460b631aaeec987eb66183
shaunster0 <shaunwerkhoven@gmail.com>
2017-08-12 22:33:59 +1000

    add function to download tensorflow model zoo

--- recognition_server.py
+++ recognition_server.py
@@ -169,7 +169,27 @@ def delete_img(img_id):
     return jsonify({'result': True})
 
 
+def download_and_extract_model_if_needed():
+  """Download and extract model tar file."""
+  dest_directory = FLAGS.model_dir
+  if not os.path.exists(dest_directory):
+    os.makedirs(dest_directory)
+  filename = DATA_URL.split('/')[-1]
+  filepath = os.path.join(dest_directory, filename)
+  if not os.path.exists(filepath):
+    def _progress(count, block_size, total_size):
+      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+          filename, float(count * block_size) / float(total_size) * 100.0))
+      sys.stdout.flush()
+    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+    print()
+    statinfo = os.stat(filepath)
+    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+  tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
 def main(_):
+    download_and_extract_model_if_needed()
     app.run(host = '0.0.0.0')
 
 
